37747047818069c7e99fd6bb378f6300b88995bf
infinitysky <dreamli1121@gmail.com>
2022-05-04 19:12:11 +1000

    M change OKAMI Printer setting M change DDA Auto Startup

--- Windows/DDA/Hospitality/resetAutoStartup.cmd
+++ Windows/DDA/Hospitality/resetAutoStartup.cmd
@@ -1,3 +1,3 @@
 reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /v ZiiPOSClassic /f
 
-SCHTASKS /CREATE /SC ONLOGON /TN "ZiiPOSClassicRetail" /TR "'C:\Program Files (x86)\ZiiForce\ZiiPOSClassic\run.bat'" /RL HIGHEST /f
+SCHTASKS /CREATE /SC ONLOGON /TN "ZiiPOSClassic" /TR "'C:\Program Files (x86)\ZiiForce\ZiiPOSClassic\run.bat'" /RL HIGHEST /f
