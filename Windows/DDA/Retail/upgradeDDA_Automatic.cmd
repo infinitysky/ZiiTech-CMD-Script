f18528b03df49e34d0befae64faf9e6127368cef
Cliff Li <dreamli1121@gmail.com>
2022-06-06 13:30:04 +1000

    *M fix download bug for upgradeDDA script (download method)

--- Windows/DDA/Retail/upgradeDDA_Automatic.cmd
+++ Windows/DDA/Retail/upgradeDDA_Automatic.cmd
@@ -3,43 +3,22 @@ cd C:\Ziitech
 
 
 echo Set-ExecutionPolicy RemoteSigned> downloadDDA.ps1
-
 echo [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 >> downloadDDA.ps1
-echo|set /p "$" >> downloadDDA.ps1
-echo|set /p "response = " >> downloadDDA.ps1
-echo|set /p "Invoke-WebRequest 'https://wombat-api.ziicloud.com/api/vs/client-version/version?clientName=Zii.Retail_Classic&version=1' | ConvertFrom-Json" >> downloadDDA.ps1
 
-
-echo|set /p " &version=1'" >> downloadDDA.ps1
-
-echo|set /p " &version=1'" >> downloadDDA.ps1
-echo|set ConvertFrom-Json >> downloadDDA.ps1
-
-
-set downloadCMD= $response = Invoke-WebRequest 'https://wombat-api.ziicloud.com/api/vs/client-version/version?clientName=Zii.Retail_Classic&version=1' | ConvertFrom-Json
-
-c&version=1' | ConvertFrom-Json
-
-
-set downloadCMD
-set downloadCMD=%downloadCMD:"=%
-set downloadCMD
-
-
-echo %downloadCMD% >> downloadDDA.ps1
-
+echo ^$response=Invoke-WebRequest -Uri 'https://wombat-api.ziicloud.com/api/vs/client-version/version?clientName=Zii.Retail_Classic^&version=1' -UseBasicParsing ^| ConvertFrom-Json >> downloadDDA.ps1
 
 echo $Link = $response.data.upgradeUrl >> downloadDDA.ps1
 echo $WebClient = New-Object System.Net.WebClient>> downloadDDA.ps1
 echo cd C:\Ziitech >> downloadDDA.ps1
 echo $WebClient.DownloadFile("$Link","C:\Ziitech\ZiiPOSClassicRetail.exe");>> downloadDDA.ps1
 
-PowerShell.exe -ExecutionPolicy Bypass .\downloadDDA.ps1
+
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command .\downloadDDA.ps1
 
 ::AssistantServer(v1.2.1)
 taskkill /IM "AssistantServer(v1.2.1).exe" /F
 taskkill /IM "ZiiPOSClassicRetail.exe" /F
-::ZiiPOSClassicRetail.exe /S
+ZiiPOSClassicRetail.exe /S
 
 DEL ZiiPOSClassicRetail.exe
 DEL downloadDDA.ps1
---
+++ Windows/ZiiPOS/ReSyncFailedItems.cmd
@@ -0,0 +1,15 @@
+@echo off
+if not exist C:\Ziitech mkdir C:\Ziitech
+cd C:\Ziitech
+
+set /p dbName="Enter Database Name: "
+echo USE %dbName% ;>resync.sql
+
+echo update SyncTableLog set FailCount^=0 where FailCount^>^=10; >>resync.sql
+
+
+"C:\Program Files\Microsoft SQL Server\100\Tools\Binn\sqlcmd.exe" -S localhost\SQLEXPRESS2008R2 -i resync.sql
+
+DEL resync.sql
+
+pause
