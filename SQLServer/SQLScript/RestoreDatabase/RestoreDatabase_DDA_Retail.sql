3f4806f75fbf7f322864816ff474b16afd73dd32
Cliff Li <dreamli1121@gmail.com>
2022-07-05 03:17:12 +1000

    2

---
+++ SQLServer/SQLScript/RestoreDatabase/RestoreDatabase_DDA_Retail.sql
@@ -0,0 +1,19 @@
+truncate table InvoiceItem;
+truncate table InvoiceHead;
+truncate table RecvAcct;
+truncate table DrawerOpenRecordTable;
+truncate table SyncDataTable;
+truncate table SyncTableLog;
+truncate table Attendance;
+truncate table CashFloatTable;
+truncate table StockTakeHead;
+truncate table CashDeclaration;
+truncate table StockReceiveHead;
+truncate table StockReceiveItem;
+truncate table StockTakeItem;
+truncate table PurchaseHead;
+truncate table PurchaseItem;
+truncate table HoldHead;
+truncate table HoldItem;
+
+
