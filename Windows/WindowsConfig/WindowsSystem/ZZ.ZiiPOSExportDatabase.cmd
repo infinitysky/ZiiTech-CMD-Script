b733e84eef31d7f0546046450989b472d743d0fe
Cliff Li <dreamli1121@gmail.com>
2023-10-03 15:24:52 +1100

    no message

--- Windows/WindowsConfig/WindowsSystem/ZZ.ZiiPOSExportDatabase.cmd
+++ Windows/WindowsConfig/WindowsSystem/ZZ.ZiiPOSExportDatabase.cmd
@@ -1,15 +1,19 @@
+::V1.1
+
 @echo off
 if not exist C:\Ziitech mkdir C:\Ziitech
 cd C:\Ziitech
 if not exist C:\Ziitech\backup mkdir C:\Ziitech\backup\
 cd C:\Ziitech
 
+DEL C:\Ziitech\backup\backupdb.bak
+
 echo Set-ExecutionPolicy RemoteSigned> run.ps1
 echo ^$response=Get-Content ^'C:\Program Files (x86)\ZiiForce\Zii.LocalServer\profilesettings.json^' ^| ConvertFrom-Json >> run.ps1
 echo ^$dbName=^$response^.ProfileSetting^.InitialCatalog >> run.ps1
 
 
-echo ^echo ^"USE $dbname;^"^>resync.sql >> run.ps1
+echo ^echo ^"USE $dbname;^"^>backupDB.sql >> run.ps1
 echo ^echo "BACKUP DATABASE $dbname  TO DISK = 'C:\Ziitech\backup\backupdb.bak';" ^>^>backupDB.sql >> run.ps1
 
 
